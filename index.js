761c9f768ac3e11fccce36265677d2a8f5811a88
LordDanklin <dcurrier585@gmail.com>
2023-02-10 20:59:55 -0500

    added inquirer prompts and changed dependency

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const mysql = require('mysql2');
 const figlet = require('figlet');
 const gradient = require('gradient-string');
+const inquirer = require('inquirer');
 require('dotenv').config();
 
 const db = mysql.createConnection({
@@ -10,6 +11,70 @@ const db = mysql.createConnection({
     user: process.env.username,
     password: process.env.password,
     database: process.env.database
+});
+
+inquirer.prompt([{
+    type: 'list',
+    name: 'options',
+    message: 'Welcome to Employee Tracker! What would you like to do?',
+    choices: [
+        "View all Departments",
+        "View all Roles",
+        "View all Employees",
+        "Add a Department",
+        "Add a Role",
+        "Add an Employee",
+        "Update Employee Managers",
+        "View Employees by Department",
+        "View Employees by Manager",
+        "Delete Department",
+        "Delete Role",
+        "Delete Employee",
+        "Quit",
+
+    ],
+}]).then(answers => {
+    switch (answers.options) {
+        case "View all departments":
+            viewAllDepartments();
+            break;
+        case "View all roles":
+            viewAllRoles();
+            break;
+        case "View all employees":
+            viewAllEmployees();
+            break;
+        case "Add a department":
+            addDepartment();
+            break;
+        case "Add a role":
+            addRole();
+            break;
+        case "Add an employee":
+            addEmployee();
+            break;
+        case "Update Employee Managers":
+            updateEmployeeManagers();
+            break;
+        case "View Employees by Department":
+            viewByDepartment();
+            break;
+        case "View Employees by Manager":
+            viewByManager();
+            break;
+        case "Delete Department":
+            deleteDepartment();
+            break;
+        case "Delete Role":
+            deleteRole();
+            break;
+        case "Delete Employee":
+            deleteEmployee();
+            break;
+        case "Quit":
+            db.end()
+            console.log("Bye")
+    }
 });
 
 figlet.text('Employee Tracker',
